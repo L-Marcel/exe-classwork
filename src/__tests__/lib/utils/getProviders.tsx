93839542a291e1d5ae18a9aa5dc8a302b2547061
LMarcel <lmgh1312@gmail.com>
2022-04-24 22:47:32 -0300

    tests bug fix

--- src/__tests__/lib/utils/getProviders.tsx
+++ src/__tests__/lib/utils/getProviders.tsx
@@ -1,12 +1,16 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import { domAnimation } from "framer-motion";
+import { QueryClient } from "react-query";
 import { Layout } from "../../../components/Layout";
 import { AllProviders } from "../../../contexts/AllProviders";
 import { theme } from "../../../theme/default";
 
+const client = new QueryClient();
+
 function getProvidersWrapper() {
   return ({ children }): JSX.Element => (
     <AllProviders 
+      client={client}
       features={domAnimation}
       reducedMotion="user"
       theme={theme}
@@ -21,3 +25,4 @@ function getProvidersWrapper() {
 };
 
 export { getProvidersWrapper };
+
